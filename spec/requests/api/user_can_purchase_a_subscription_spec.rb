e284e7c8e7a9931e0d3ecc055d483136833084ab
Thomas <thomas@craftacademy.se>
2020-03-30 08:30:28 +0200

    implements subscription purchase flow (first iteration)

--- spec/requests/api/user_can_purchase_a_subscription_spec.rb
+++ spec/requests/api/user_can_purchase_a_subscription_spec.rb
@@ -2,18 +2,22 @@
 
 RSpec.describe 'POST api/subscriptions', type: :request do
   before do
-    post '/api/subscriptions', params: { stripeToken: '123434', email: 'thomas@craft.com' }
+    post '/api/subscriptions', params: { stripeToken: 'tok_1GRG6ZAW8HWEjAbEX381j7SB', email: 'thomas@mail2.com' }
   end
 
   it 'responds with success status' do
     expect(response_json).to eq JSON.parse({ status: 'paid' }.to_json)
   end
 
+  it 'creates or retrieves a customer on stripe' do
+    expect(Stripe::Customer.list.data.first.email).to eq 'thomas@mail2.com'
+  end
+
   it 'creates a subscription for a specific user' do
-    # check the customer entry on stripe to have thomas@craft.com as email
+    expect(Stripe::Customer.list(email: 'thomas@mail2.com').first.subscriptions).to be_truthy
   end
 
   it 'subscription for a specific user is a specific subscription plan' do
-    # check if the customer has purchased a subscription plan
+    expect(Stripe::Customer.list(email: 'thomas@mail2.com').first.subscriptions.first.plan.id).to eq 'basic_subscription'
   end
 end
